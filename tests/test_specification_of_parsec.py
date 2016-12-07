9c0f378f2055e8bdb663fbed7248a545d936fd09
Tommy Vagbratt <tommy@vagbratt.se>
2016-12-07 11:28:58 +0100

    add test for letter parser

--- tests/test_specification_of_parsec.py
+++ tests/test_specification_of_parsec.py
@@ -16,13 +16,22 @@ from parsec import *
 
 class ParsecSpecificationTest(unittest.TestCase):
     '''Test the specification of parsec.py'''
-    def test_times_with_then(self):
+    def test_times_with_then(_):
         parser = times(letter(), 3) >> digit()
-        self.assertEqual(parser.parse('xyz1'), '1')
-        self.assertRaises(ParseError, parser.parse, 'xy1')
-        self.assertRaises(ParseError, parser.parse, 'xyz')
-        self.assertRaises(ParseError, parser.parse, 'xyzw')
+        _.assertEqual(parser.parse('xyz1'), '1')
+        _.assertRaises(ParseError, parser.parse, 'xy1')
+        _.assertRaises(ParseError, parser.parse, 'xyz')
+        _.assertRaises(ParseError, parser.parse, 'xyzw')
 
+    def test_letter_parser(_):
+        parser = letter()
+        for c in "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz":
+            _.assertEqual(parser.parse(c), c)
+        for c in "0123456789+-.,;:?^*":
+            _.assertRaises(ParseError, parser.parse, c)
+
+        _.assertEqual(parser.parse('xyz1'), 'x')
+        _.assertRaises(ParseError, parser.parse, '42')
 
 
 if __name__ == '__main__':
