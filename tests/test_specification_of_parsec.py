fcca3fdb4620ede2d2dde5b2e8985665df1aad6b
Tommy Vagbratt <tommy@vagbratt.se>
2016-12-07 15:26:07 +0100

    use " instead of '

--- tests/test_specification_of_parsec.py
+++ tests/test_specification_of_parsec.py
@@ -3,11 +3,11 @@
 
 from __future__ import generators
 
-'''
+"""
 Test the specification of parsec.py.
-'''
+"""
 
-__author__ = 'Daniel Edin, Micael Loberg and Tommy Vagbratt'
+__author__ = "Daniel Edin, Micael Loberg and Tommy Vagbratt"
 
 import random
 import unittest
@@ -169,24 +169,24 @@ class TommyTest(unittest.TestCase):
 
 
 class DanielTest(unittest.TestCase):
-    '''Test the implementation of Text.Parsec.Char.'''
+    """Test the implementation of Text.Parsec.Char."""
 
     def test_letter(self):
         parser = letter()
-        self.assertEqual(parser.parse('A'), 'A')
-        self.assertRaises(ParseError, parser.parse, '9')
+        self.assertEqual(parser.parse("A"), "A")
+        self.assertRaises(ParseError, parser.parse, "9")
 
     def test_many(self):
         parser = many(letter())
-        self.assertEqual(parser.parse(''), [])
-        self.assertEqual(parser.parse('x'), ['x'])
-        self.assertEqual(parser.parse('abcO'), ['a', 'b', 'c', 'O'])
+        self.assertEqual(parser.parse(""), [])
+        self.assertEqual(parser.parse("x"), ["x"])
+        self.assertEqual(parser.parse("abcO"), ["a", "b", "c", "O"])
 
     def test_many1(self):
         parser = many1(letter())
-        self.assertEqual(parser.parse('x'), ['x'])
-        self.assertEqual(parser.parse('abcO'), ['a', 'b', 'c', 'O'])
-        self.assertRaises(ParseError, parser.parse, '*')
+        self.assertEqual(parser.parse("x"), ["x"])
+        self.assertEqual(parser.parse("abcO"), ["a", "b", "c", "O"])
+        self.assertRaises(ParseError, parser.parse, "*")
 
     def test_mark(self):
         parser = many(mark(many(letter())) << (string(";") | string("\n")))
@@ -197,61 +197,61 @@ class DanielTest(unittest.TestCase):
 
         (start, letters, end) = lines[0]
         self.assertEqual(start, (0, 0))
-        self.assertEqual(letters, ['a', 's', 'd', 'f'])
+        self.assertEqual(letters, ["a", "s", "d", "f"])
         self.assertEqual(end, (0, 4))
 
         (start, letters, end) = lines[1]
         self.assertEqual(start, (0, 5))
-        self.assertEqual(letters, ['k', 'a', 'p', 'p', 'a'])
+        self.assertEqual(letters, ["k", "a", "p", "p", "a"])
         self.assertEqual(end, (0, 10))
 
         (start, letters, end) = lines[2]
         self.assertEqual(start, (0, 11))
-        self.assertEqual(letters, ['t', 'h', 'e', 'n'])
+        self.assertEqual(letters, ["t", "h", "e", "n"])
         self.assertEqual(end, (0, 15))
 
         (start, letters, end) = lines[3]
         self.assertEqual(start, (1, 0))
-        self.assertEqual(letters, ['e', 'l', 's', 'e'])
+        self.assertEqual(letters, ["e", "l", "s", "e"])
         self.assertEqual(end, (1, 4))
 
     def test_none_of(self):
         parser = none_of("()[]{}")
-        self.assertEqual(parser.parse('test'), 't')
-        self.assertEqual(many(parser).parse('test'), ['t', 'e', 's', 't'])
-        self.assertRaises(ParseError, parser.parse, '[]')
+        self.assertEqual(parser.parse("test"), "t")
+        self.assertEqual(many(parser).parse("test"), ["t", "e", "s", "t"])
+        self.assertRaises(ParseError, parser.parse, "[]")
 
     def test_one_of(self):
         parser = one_of("()[]{}")
-        self.assertEqual(parser.parse('()'), '(')
-        self.assertEqual(many(parser).parse('()'), ['(', ')'])
-        self.assertRaises(ParseError, parser.parse, 'y')
+        self.assertEqual(parser.parse("()"), "(")
+        self.assertEqual(many(parser).parse("()"), ["(", ")"])
+        self.assertRaises(ParseError, parser.parse, "y")
 
     def test_parse(self):
-        self.assertEqual(letter().parse("x"), 'x')
-        self.assertEqual(letter().parse("yX"), 'y')
-        self.assertRaises(ParseError, letter().parse, '2')
+        self.assertEqual(letter().parse("x"), "x")
+        self.assertEqual(letter().parse("yX"), "y")
+        self.assertRaises(ParseError, letter().parse, "2")
 
     # TODO
    # def test_parse_operator(self):
-   #     self.assertEqual(parse(letter(), "x", 0), 'x')
+   #     self.assertEqual(parse(letter(), "x", 0), "x")
 
 
     def test_parse_partial(self):
-        self.assertEqual(letter().parse_partial("x_rest"), ('x', '_rest'))
+        self.assertEqual(letter().parse_partial("x_rest"), ("x", "_rest"))
         self.assertRaises(ParseError, letter().parse_partial, "1")
 
     def test_parse_strict(self):
-        self.assertEqual(letter().parse_strict("x"), 'x')
-        self.assertEqual(string("").parse_strict(""), '')
+        self.assertEqual(letter().parse_strict("x"), "x")
+        self.assertEqual(string("").parse_strict(""), "")
         self.assertRaises(ParseError, letter().parse_strict, "1")
 
     def test_parsecmap(self):
         parser = letter().parsecmap(str.upper)
-        self.assertEqual(parser.parse("x"), 'X')
+        self.assertEqual(parser.parse("x"), "X")
 
     def test_parsecmap_operator(self):
-        self.assertEqual(parsecmap(letter(), str.upper).parse("x"), 'X')
+        self.assertEqual(parsecmap(letter(), str.upper).parse("x"), "X")
 
     def test_result(self):
 
@@ -261,13 +261,13 @@ class DanielTest(unittest.TestCase):
         self.assertRaises(ParseError, parser.parse, "1")
 
     def test_result_operator(self):
-        self.assertEqual(result(letter(), "New result").parse("x"), 'New result')
+        self.assertEqual(result(letter(), "New result").parse("x"), "New result")
 
     def test_regex(self):
-        parser = regex(r'[a-z]')
-        self.assertEqual(parser.parse('abc'), 'a')
-        self.assertEqual(parser.parse('x'), 'x')
-        self.assertRaises(ParseError, parser.parse, '2')
+        parser = regex(r"[a-z]")
+        self.assertEqual(parser.parse("abc"), "a")
+        self.assertEqual(parser.parse("x"), "x")
+        self.assertRaises(ParseError, parser.parse, "2")
 
 
 
@@ -275,13 +275,13 @@ class DanielTest(unittest.TestCase):
 
 
 class ParsecSpecificationTest(unittest.TestCase):
-    '''Test the specification of parsec.py'''
+    """Test the specification of parsec.py"""
     def test_times_with_then(_):
         parser = times(letter(), 3) >> digit()
-        _.assertEqual(parser.parse('xyz1'), '1')
-        _.assertRaises(ParseError, parser.parse, 'xy1')
-        _.assertRaises(ParseError, parser.parse, 'xyz')
-        _.assertRaises(ParseError, parser.parse, 'xyzw')
+        _.assertEqual(parser.parse("xyz1"), "1")
+        _.assertRaises(ParseError, parser.parse, "xy1")
+        _.assertRaises(ParseError, parser.parse, "xyz")
+        _.assertRaises(ParseError, parser.parse, "xyzw")
 
     def test_letter_parser(_):
         parser = letter()
@@ -290,9 +290,9 @@ class ParsecSpecificationTest(unittest.TestCase):
         for c in "0123456789+-.,;:?^*":
             _.assertRaises(ParseError, parser.parse, c)
 
-        _.assertEqual(parser.parse('xyz1'), 'x')
-        _.assertRaises(ParseError, parser.parse, '42')
+        _.assertEqual(parser.parse("xyz1"), "x")
+        _.assertRaises(ParseError, parser.parse, "42")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
