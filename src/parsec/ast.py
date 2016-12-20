6d45bd6e8dac113d3da4ea697d946d89b5b8ea64
Tommy Vagbratt <tommy@vagbratt.se>
2016-12-20 15:26:43 +0100

    refactor ast nodes

--- src/parsec/ast.py
+++ src/parsec/ast.py
@@ -2,27 +2,25 @@
 # Abstract Syntax Tree
 #=================================================================
 
-class Comment:
+class Node:
+    def __repr__(self):
+        return "<{0}>".format(self)
+
+class Comment(Node):
     def __init__(self, message):
         self.message = message
 
     def __str__(self):
         return "# {0}".format(self.message)
 
-    def __repr__(self):
-        return "<{0}>".format(self)
-
-class Start:
+class Start(Node):
     def __init__(self, start_rule):
         self.start_rule = start_rule
 
     def __str__(self):
         return "start = {0};".format(self.start_rule)
 
-    def __repr__(self):
-        return "<{0}>".format(self)
-
-class Token:
+class Token(Node):
     def __init__(self, name, value):
         self.name = name
         self.value = value
@@ -30,5 +28,3 @@ class Token:
     def __str__(self):
         return "token {0} = \"{1}\";".format(self.name, self.value)
 
-    def __repr__(self):
-        return "<{0}>".format(self)
