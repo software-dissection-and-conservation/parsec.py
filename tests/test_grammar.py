d13671e6373f71e05ec11152fe876abac9790bd5
Tommy Vagbratt <tommy@vagbratt.se>
2016-12-20 15:26:44 +0100

    create test and implementation files for grammar

---
+++ tests/test_grammar.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Test the grammar parser
+"""
+
+__author__ = "Daniel Edin, Micael Loberg and Tommy Vagbratt"
+
+import unittest
+
+class GrammarTest(unittest.TestCase):
+    """Test the grammar parser"""
+    pass
+
+
+
+if __name__ == "__main__":
+    unittest.main()
