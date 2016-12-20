424a63d4870be08e4adfa33ca874a170b9f3867c
Tommy Vagbratt <tommy@vagbratt.se>
2016-12-20 15:26:43 +0100

    create files for test and implementation of the ast

---
+++ tests/test_ast.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+"""
+Test the Abstract Syntax Tree
+"""
+
+__author__ = "Daniel Edin, Micael Loberg and Tommy Vagbratt"
+
+import random
+import unittest
+
+from parsec import *
+from parsec.ast import *
+
+class AbstractSyntaxTreeTest(unittest.TestCase):
+    """Test the Abstract Syntax Tree"""
+    pass
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
