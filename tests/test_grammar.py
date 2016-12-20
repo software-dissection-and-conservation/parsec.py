12e01157825cbebdd4c5e857c0dfc74f9cd98efd
Tommy Vagbratt <tommy@vagbratt.se>
2016-12-20 15:26:44 +0100

    test and implementation for many_of parser

--- tests/test_grammar.py
+++ tests/test_grammar.py
@@ -9,9 +9,35 @@ __author__ = "Daniel Edin, Micael Loberg and Tommy Vagbratt"
 
 import unittest
 
+from pprint import pprint
+from parsec.grammar import *
+
 class GrammarTest(unittest.TestCase):
     """Test the grammar parser"""
-    pass
+
+    def test_many_of(self):
+        p = many_of("xyz")
+        valid = [ ""
+                , "x"
+                , "y"
+                , "z"
+                , "xyz"
+                , "xxxxxxxxxxxyyyyyyyyyzzzzzz"
+                , "yxyxyxyxyxyxzxyzxyyzxyzxyzyxyzxyzyxyzyyxyyxzyyzyxzyxyxxzz"
+                ]
+        for s in valid:
+            self.assertEqual(p.parse_strict(s), list(s))
+
+        invalid = [ "a"
+                  , "b"
+                  , "c"
+                  , "sdfg"
+                  , "2342"
+                  , "fdkljdfjfdljaskjdfhaohofihoiehfihfsd"
+                  , "With great power, comes...    awesomeness!"
+                  ]
+        for s in invalid:
+            self.assertEqual(p.parse(s), [])
 
 
 
