c04d9c8af6f623dce308010723502471ec18fc89
Tommy Vagbratt <tommy@vagbratt.se>
2016-12-20 15:26:43 +0100

    implement comment node

--- src/parsec/ast.py
+++ src/parsec/ast.py
@@ -2,3 +2,9 @@
 # Abstract Syntax Tree
 #=================================================================
 
+class Comment:
+    def __init__(self, message):
+        self.message = message
+
+    def __str__(self):
+        return "# {0}".format(self.message)
