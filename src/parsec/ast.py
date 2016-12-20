6ec3c0b4700c7bd0caca31a1151da30c198c5da8
Tommy Vagbratt <tommy@vagbratt.se>
2016-12-20 15:26:43 +0100

    make ast nodes easier to inspect

--- src/parsec/ast.py
+++ src/parsec/ast.py
@@ -9,9 +9,15 @@ class Comment:
     def __str__(self):
         return "# {0}".format(self.message)
 
+    def __repr__(self):
+        return "<{0}>".format(self)
+
 class Start:
     def __init__(self, start_rule):
         self.start_rule = start_rule
 
     def __str__(self):
         return "start = {0};".format(self.start_rule)
+
+    def __repr__(self):
+        return "<{0}>".format(self)
