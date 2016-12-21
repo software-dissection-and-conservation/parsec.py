f15290f099a2e49238488481944d02fb697cd964
Tommy Vagbratt <tommy@vagbratt.se>
2016-12-21 18:31:04 +0100

    test for grammar node

--- tests/test_ast.py
+++ tests/test_ast.py
@@ -59,5 +59,61 @@ class AbstractSyntaxTreeTest(unittest.TestCase):
         self.assertEqual(rule.productions, [Production([String("var"), Identifier("identifier"), String(";")]), Production([String("val"), Identifier("identifier"), String(";")])])
         self.assertEqual(str(rule), "declaration = \"var\" identifier \";\" | \"val\" identifier \";\";")
 
+    def test_gramar1(self):
+        g = Grammar([Comment("this is a comment!")])
+        self.assertEqual(g.declarations, [Comment("this is a comment!")])
+        self.assertEqual(g.comments(), [Comment("this is a comment!")])
+        self.assertEqual(g.tokens(), [])
+        self.assertEqual(g.rules(), [])
+        self.assertEqual(g.starts(), [])
+
+    def test_gramar2(self):
+        g = Grammar([ Comment("this is a comment!")
+                    , Start("foo")])
+        self.assertEqual(g.declarations, [ Comment("this is a comment!")
+                                         , Start("foo")])
+        self.assertEqual(g.tokens(), [])
+        self.assertEqual(g.comments(), [Comment("this is a comment!")])
+        self.assertEqual(g.tokens(), [])
+        self.assertEqual(g.rules(), [])
+        self.assertEqual(g.starts(), [Start("foo")])
+
+    def test_gramar3(self):
+        declarations = [ Comment("This is a grammar")
+                       , Comment("Here we define some tokens")
+                       , Token("a", String("x"))
+                       , Token("b1", String("x1"))
+                       , Token("b2", String("x2"))
+                       , Comment("And here come the grammar rules")
+                       , Rule("foo", [ Production([Identifier("a"), Identifier("other")]) , Production([Identifier("_")]) ])
+                       , Rule("other", [ Production([Identifier("b1")]) , Production([Identifier("b2")]) , Production([String("hey!")]) ])
+                       , Comment("Please start at foo")
+                       , Start("foo")
+                       ]
+        g = Grammar([ Comment("This is a grammar")
+                    , Comment("Here we define some tokens")
+                    , Token("a", String("x"))
+                    , Token("b1", String("x1"))
+                    , Token("b2", String("x2"))
+                    , Comment("And here come the grammar rules")
+                    , Rule("foo", [ Production([Identifier("a"), Identifier("other")]) , Production([Identifier("_")]) ])
+                    , Rule("other", [ Production([Identifier("b1")]) , Production([Identifier("b2")]) , Production([String("hey!")]) ])
+                    , Comment("Please start at foo")
+                    , Start("foo")
+                    ])
+        self.assertEqual(g.declarations, declarations)
+        self.assertEqual(g.comments(), [ Comment("This is a grammar")
+                                       , Comment("Here we define some tokens")
+                                       , Comment("And here come the grammar rules")
+                                       , Comment("Please start at foo")
+                                       ])
+        self.assertEqual(g.rules(), [ Rule("foo", [ Production([Identifier("a"), Identifier("other")]) , Production([Identifier("_")]) ])
+                                    , Rule("other", [ Production([Identifier("b1")]) , Production([Identifier("b2")]) , Production([String("hey!")]) ])
+                                    ])
+        self.assertEqual(g.starts(), [ Start("foo") ])
+        self.assertEqual(g.tokens(), [ Token("a", String("x")) , Token("b1", String("x1")) , Token("b2", String("x2")) ])
+
+
+
 if __name__ == "__main__":
     unittest.main()
